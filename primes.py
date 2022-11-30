0bf02793d965c2fcb1a054d541b09e19cb02513b
Ziwen Wang <eloisewong0725@gmail.com>
2022-11-30 03:30:23 +0000

    list-of-primes-v2

--- primes.py
+++ primes.py
@@ -3,4 +3,22 @@
 
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes <= 0:
+        raise ValueError
+
+    a = 0
+    b = 2
+    prime = True
+    while len(list) < number_of_primes:
+            for c in range(2,b):
+                if b % c == 0:
+                    prime = False
+
+    if prime:
+        list.append(b)
+        a += 1
+    b += 1
+    prime = True
+
     return list
